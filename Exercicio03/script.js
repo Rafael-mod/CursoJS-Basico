55932201b371314a5256942c16c6f206f063ef2e
Rafael Modesto <rafaelmodestodafonseca@gmail.com>
2025-07-18 13:59:56 -0300

    Alterações finais

--- Exercicio03/script.js
+++ Exercicio03/script.js
@@ -4,15 +4,16 @@ const num1txt = document.getElementById("num");
 const inicio = Number(num1txt.value)
 const num2txt = document.getElementById("numfim");
 const fim = Number(num2txt.value);
-const restxt = document.getElementById("resposta");
-let resultado = ''
 const passotxt = document.getElementById("numpassos");
 let passo = Number(passotxt.value);
+const restxt = document.getElementById("resposta");
+let resultado = ''
 
 if (passo <= 0) {
     alert('Passo inválido! Considerando passo 1');
     passo = 1;
 }
+
 (inicio < fim || inicio < 0 || fim < 0) ? alert('Dados válidos') : alert('Dados inválidos');
 for (let i = inicio; i <= fim; i+=passo){
         if (i == fim){
@@ -21,4 +22,5 @@ for (let i = inicio; i <= fim; i+=passo){
             resultado += `${i} 👉 `;
         }
 }restxt.innerHTML = `Contando de ${inicio} até ${fim}: <br> 👉${resultado} `;
+
 } 
