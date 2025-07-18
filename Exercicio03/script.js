7f9e178e5aa8b0f13f983b3cbeeb662c4bfd7ae7
Rafael Modesto <rafaelmodestodafonseca@gmail.com>
2025-07-18 13:33:30 -0300

    Terminei o exercicio básico

--- Exercicio03/script.js
+++ Exercicio03/script.js
@@ -3,17 +3,21 @@ function Contar(){
 const num1txt = document.getElementById("num");
 const inicio = Number(num1txt.value)
 const num2txt = document.getElementById("numfim");
-let fim = Number(num2txt.value);
+const fim = Number(num2txt.value);
 const restxt = document.getElementById("resposta");
 let resultado = ''
+const passotxt = document.getElementById("numpassos");
+const passo = Number(passotxt.value);
 
 if ( fim == 0) {
     alert('Erro! Digite um número válido.')
-} else{
-for (let i = inicio; i <= fim; i++){
-    resultado +=` ${i}`;
-} restxt.innerHTML = `Contando de ${inicio} até ${fim}: <br> ${resultado}`;
-
-}
+} else {
+    for (let i = inicio; i <= fim; i+=passo){
+        if (i == fim){
+            resultado += `${i}🏁`;
+        } else {
+            resultado += `${i} 👉 `;
+        }
+}} restxt.innerHTML = `Contando de ${inicio} até ${fim}: <br> 👉${resultado} `;
 
 }
