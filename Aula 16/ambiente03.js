a6d2752c182907a51f4077c798247d9766640145
Rafael Modesto <rafaelmodestodafonseca@gmail.com>
2025-07-21 10:18:46 -0300

    Aula 16

---
+++ Aula 16/ambiente03.js
@@ -0,0 +1,5 @@
+let v = function(x) {
+    return x * 2;
+}
+
+console.log(`O resultado é ${v(5)}`)
---
+++ Aula 16/ambienteo2.js
@@ -0,0 +1,5 @@
+function soma(a=0, b=0){
+return a + b;
+}
+
+console.log(`A soma é ${soma(10,5)}`)
